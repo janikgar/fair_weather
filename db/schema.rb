104aa9cb5c16a63c5e0aec4fff5b1f8df63409c9
janikgar <janikgar@gmail.com>
2018-08-24 15:53:44 -0500

    migrated to postgresql database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_25_025843) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cities", force: :cascade do |t|
     t.string "city"
     t.datetime "created_at", null: false
